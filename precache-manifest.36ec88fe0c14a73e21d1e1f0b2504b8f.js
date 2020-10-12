7e3fcc27fa1b72646d78b17a999d89c14cc7be2d
Robert James <36674828+roborbob@users.noreply.github.com>
2020-10-12 15:12:43 +0100

    Updates

---
+++ precache-manifest.36ec88fe0c14a73e21d1e1f0b2504b8f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df563d416466ae491760bf67bbd47e48",
+    "url": "/automatic/index.html"
+  },
+  {
+    "revision": "750a67098fd4a4140759",
+    "url": "/automatic/static/css/main.a9485a04.chunk.css"
+  },
+  {
+    "revision": "660668f466dd788a897a",
+    "url": "/automatic/static/js/2.98010e77.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/automatic/static/js/2.98010e77.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "750a67098fd4a4140759",
+    "url": "/automatic/static/js/main.48b126d8.chunk.js"
+  },
+  {
+    "revision": "bf190a9f1987a4657e5f",
+    "url": "/automatic/static/js/runtime-main.7ec6d23a.js"
+  }
+]);
